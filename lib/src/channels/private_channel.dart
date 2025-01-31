78216b53d289733084ef8af8d411dc0868af7c4f
dipflix <vakardhuk.dmitriy.03@gmail.com>
2025-01-31 16:16:14 +0200

    revert prefix to channels

--- lib/src/channels/private_channel.dart
+++ lib/src/channels/private_channel.dart
@@ -84,9 +84,9 @@ class PrivateChannel extends EndpointAuthorizableChannel<PrivateChannelState,
     required this.publicStreamGetter,
     required this.publicEventEmitter,
     required this.connectionDelegate,
-    required String name,
+    required this.name,
     required this.authorizationDelegate,
-  }) : name = 'private-$name';
+  });
 
   /// Unlike the public channels, this channel:
   /// 1. Grabs the authorization data of type [PrivateChannelAuthorizationData].
@@ -101,7 +101,6 @@ class PrivateChannel extends EndpointAuthorizableChannel<PrivateChannelState,
     final fixatedLifeCycleCount = startNewAuthRequestCycle();
     await setAuthKeyFromDelegate();
     final currentAuthKey = authData?.authKey;
-
     if (fixatedLifeCycleCount < authRequestCycle ||
         currentAuthKey == null ||
         state?.status == ChannelStatus.unsubscribed) {
